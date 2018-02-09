5d4dabf43110acb5e6f27a5c31cb0ab5a21bf5db
stayasleep <virtuallyinfekted@gmail.com>
2018-02-08 23:53:09 -0800

    ch3 done this time

--- src/ch03/EvilEmpire.java
+++ src/ch03/EvilEmpire.java
@@ -2,16 +2,10 @@ package ch03;
 import java.net.*;
 
 public class EvilEmpire {
-    static int i=99;
-    static double d=5.43;
-    static double pi= 3.14_159_265_358;
 
     public static void main(String[] args) throws Exception{
         try{
-//            Socket s = new Socket("35.161.100.111",80);
-            System.out.println(i);
-            System.out.println(d);
-            System.out.println(pi);
+            Socket s = new Socket("11.111.111.111",80);
             System.out.println("Am connected");
         }catch (SecurityException e){
             System.out.println("SecurityException: Could Not Connect..");
